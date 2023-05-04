69f4b69a4bfc560b529defd2233054fbd9da1e26
H <hrvojep@yahoo.com>
2023-05-04 19:59:04 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,12 +16,42 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
 
-  acl = "public-read"
+
+# resource "aws_s3_bucket_acl" "bucket" {
+#   bucket = aws_s3_bucket.bucket.id
+
+#   acl = "public-read"
+# }
+
+#
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
+}
+
+#
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.bucket.id
   policy = <<EOF
@@ -43,6 +73,10 @@ resource "aws_s3_bucket_policy" "policy" {
 }
 EOF
 }
+
+
+
+
 
 resource "aws_s3_object" "webapp" {
   acl          = "public-read"
